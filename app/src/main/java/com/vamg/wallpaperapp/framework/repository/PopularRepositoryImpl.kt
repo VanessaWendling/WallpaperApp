62d641d81cb0cf31fc409087f2cfd9af6750458b
VanessaWendling <vanessamw2001@hotmail.com>
2023-09-18 18:00:39 -0300

    primeiro UseCase

---
+++ app/src/main/java/com/vamg/wallpaperapp/framework/repository/PopularRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.vamg.wallpaperapp.framework.repository
+
+import androidx.paging.PagingSource
+import com.vamg.core.data.repository.PopularRemoteDataSrc
+import com.vamg.core.data.repository.PopularRepository
+import com.vamg.core.domain.model.PhotoDomain
+import com.vamg.wallpaperapp.framework.network.response.DataWrapperResponse
+import com.vamg.wallpaperapp.framework.paging.PopularPagingSrc
+
+class PopularRepositoryImpl(private val remoteDataSrc: PopularRemoteDataSrc<DataWrapperResponse>): PopularRepository {
+    override fun fetchPopular(pages: Int): PagingSource<Int, PhotoDomain> {
+        return PopularPagingSrc(remoteDataSrc, pages);
+    }
+
+}
---
+++ core/src/main/java/com/vamg/core/usecase/popularUseCase/GetPopularUseCase.kt
@@ -0,0 +1,25 @@
+package com.vamg.core.usecase.popularUseCase
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.vamg.core.data.repository.PopularRepository
+import com.vamg.core.domain.model.PhotoDomain
+import com.vamg.core.usecase.PagingUseCase
+import kotlinx.coroutines.flow.Flow
+
+interface GetPopularUseCase{
+    operator fun invoke(params: GetPopularParams): Flow<PagingData<PhotoDomain>>
+
+    data class GetPopularParams(val pagingConfig: PagingConfig)
+}
+class GetPopularUseCaseImpl(
+    private val repository: PopularRepository
+) : PagingUseCase<GetPopularUseCase.GetPopularParams, PhotoDomain>(), GetPopularUseCase {
+    override fun createFlowObservable(params: GetPopularUseCase.GetPopularParams): Flow<PagingData<PhotoDomain>> {
+        val pagingSource = repository.fetchPopular(pages = params.pagingConfig.pageSize)
+        return Pager(config = params.pagingConfig){
+            pagingSource
+        }.flow
+    }
+}
